2dfb88b43657784bec7707fa554f37cb22aa38b1
felipe-almarques <108095964+felipe-almarques@users.noreply.github.com>
2023-06-25 13:05:10 -0300

    Update previsao.R

--- previsao.R
+++ previsao.R
@@ -17,15 +17,33 @@ spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0), include.mean = FALSE),
                    distribution = 'std')
 fit <- ugarchfit(spec, serie, solver = 'hybrid')
 
-ts.plot(serie)
-
 #### Fazendo Previsão com o Modelo ####
-
-previsao <- ugarchforecast(fit, n.ahead = 7)
-fore_serie <- previsao@forecast$seriesFor
+## Previsão da volatilidade
+previsao <- ugarchforecast(fit, n.ahead = 1)
 fore_sigma <- previsao@forecast$sigmaFor
 
-df <- data.frame(serie=as.vector(fore_serie),
-                 sigma=as.vector(fore_sigma))
+## Previsão do VaR e ES
+xf = function(x, mu_, sigma_, shape_) {
+  x*ddist(distribution = "std", 
+          y = x,
+          mu = mu_, 
+          sigma = sigma_, 
+          skew = 0,
+          shape = shape_)
+}
+
+alpha <- 0.05
+var <- qdist(distribution = "std", alpha, mu = previsao@forecast[["seriesFor"]][1], sigma = sigma(previsao)[1], skew = 0, shape = coef(fit)["shape"])
+ES = integrate(xf, 
+                -Inf, 
+                var, 
+                mu_ = previsao@forecast[["seriesFor"]][1],
+                sigma_ = sigma(previsao)[1],
+                shape_ = coef(fit)["shape"])$value/alpha
+
+#### Criando o .csv ####
+
+df <- data.frame(volatilidade = as.vector(fore_sigma),
+                 VaR = var, ES = ES)
 
 write_csv(df, paste0('data/', Sys.Date(), '_valor_predito.csv'))
